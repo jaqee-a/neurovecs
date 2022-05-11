c539c7e62b09234f40c8853a6a88af2063a74db8
roumaLch <93671096+roumaLch@users.noreply.github.com>
2022-05-11 14:01:22 +0100

    add obstacle

--- src/User.py
+++ src/User.py
@@ -7,7 +7,7 @@ import numpy as np
 
 class user :
 
-    def __init__(self, velocity, height, width) -> None:
+    def __init__(self, velocity, height, width, obs) -> None:
         
         self.height, self.width = height, width
         self.u = pygame.Vector3(random.random() * self.width* 0.5 + 200, random.random() * self.height* 0.5 + 200, 0)
@@ -16,11 +16,16 @@ class user :
         self.beta = 0
         self.delta_theta = 0.3
         self.delta_t = 100
-
-
+        self.obs = pygame.Rect(obs[0].x-50, obs[0].y-50, obs[1][0], obs[1][1])
+        
+        while self.obs.collidepoint(self.u.x, self.u.y) == True :
+            self.u = pygame.Vector3(random.random() * self.width* 0.5 + 200, random.random() * self.height* 0.5 + 200, 0)
+        
+    
     def randomWalk(self, iter) :
     
-        if iter % self.delta_t == 0 or self.u.x < 0 or self.u.x > self.width or self.u.y < 0 or self.u.y > self.height :
+        if iter % self.delta_t == 0 or self.u.x < 0 or self.u.x > self.width or self.u.y < 0 or self.u.y > self.height \
+           or self.obs.collidepoint(self.u.x, self.u.y) == True:
            a = random.randint(0, 360)
            self.theta = a * np.pi / 180
 
