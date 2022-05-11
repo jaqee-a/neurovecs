f03baa868d71de9ff5bd6d26a1f390d61fa1e61b
roumaLch <93671096+roumaLch@users.noreply.github.com>
2022-05-11 12:13:30 +0100

    Update User.py

--- src/User.py
+++ src/User.py
@@ -7,31 +7,31 @@ import numpy as np
 
 class user :
 
-    def __init__(self, h, w) -> None:
+    def __init__(self, velocity, height, width) -> None:
         
-        self.height, self.width = h, w
+        self.height, self.width = height, width
         self.u = pygame.Vector3(random.random() * self.width* 0.5 + 200, random.random() * self.height* 0.5 + 200, 0)
-        self.v = 0.01
+        self.v = velocity
         self.theta = 0
         self.beta = 0
         self.delta_theta = 0.3
         self.delta_t = 100
 
 
-    def randomWalk(self) :
+    def randomWalk(self, iter) :
     
         if iter % self.delta_t == 0 or self.u.x < 0 or self.u.x > self.width or self.u.y < 0 or self.u.y > self.height :
            a = random.randint(0, 360)
-           theta = a * np.pi / 180
+           self.theta = a * np.pi / 180
 
-        beta = random.uniform(0,1)
-        delta_theta = 0.3 - 0.6 * beta
+        self.beta = random.uniform(0,1)
+        self.delta_theta = 0.3 - 0.6 * self.beta
 
-        theta += delta_theta
+        self.theta += self.delta_theta
 
-        self.u.x += self.v * np.cos(theta) * self.delta_t
-        self.u.y += self.v * np.sin(theta) * self.delta_t
+        self.u.x += self.v * np.cos(self.theta) * self.delta_t
+        self.u.y += self.v * np.sin(self.theta) * self.delta_t
 
-        return self.u
+        
 
    
