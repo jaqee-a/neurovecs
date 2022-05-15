5b151ad77fc26876e1cbb073a11a67880ee05701
roumaLch <93671096+roumaLch@users.noreply.github.com>
2022-05-16 00:12:38 +0100

    Update simulation.py

--- src/simulation.py
+++ src/simulation.py
@@ -4,6 +4,7 @@ from math import sqrt
 import pygame
 import numpy as np
 from User import user
+from drone import drone
 
 
 from scipy import rand
@@ -31,12 +32,11 @@ for _ in range(user_n):
     p = user(0.001, height, width, obs)
     pop.append([p, 0])
 
-drone = []
+drone_l = []
 for _ in range(drone_n):
-    d = pygame.Vector3(50, 50, 20)
-    drone.append(d)
+    d = drone(theta)
+    drone_l.append(d)
 
-dm = pygame.Vector3(0,0,0)
 stable = False
 iter = 0
 
@@ -48,81 +48,74 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
-    
 
     non_con_user = 100
     
     for p in pop :
             p[1] = 0
-            for i, d in enumerate(drone):
-                di = d.distance_to(p[0].u)
-                if not p[1] and di < dist :
+            for i, d in enumerate(drone_l):
+                di = d.p.distance_to(p[0].u)
+                if not p[1] and di < d.distance() :
                     p[1] = 1
                     non_con_user -= 1
+                    d.n_users += 1
+                    break
 
     stable = True
-    for d in drone:
+    for d in drone_l:
 
+        # Attraction force with the users
         F1 = pygame.Vector3(0, 0, 0)
         
         for p in pop:
             if not p[1] :
-                v = p[0].u - d
+                v = p[0].u - d.p
                 F1 += v.normalize() * 1 / (v.length() * (user_n/drone_n))
                
-
+        # Repulsion force with other drones
         F2 = pygame.Vector3(0, 0, 0)
 
-        for dr in drone:
-            if d != dr : 
-                v = d - dr
+        for dr in drone_l:
+            if d.p != dr.p : 
+                v = d.p - dr.p
                 F2 += v.normalize() * 1 / (v.length()*7)
             
             
-        
+        # Repulsion force with the obstacles
         F3 = pygame.Vector3(0, 0, 0)
 
         for ob in obs :
-            v = d - ob[0]
+            v = d.p - ob[0]
             F3 += v.normalize() * 1 / (v.length()*2)
 
-        F4 = pygame.Vector3(0, 0, 1) * (1 / d.z**2)
+        # Repulsion force with the ground
+        F4 = pygame.Vector3(0, 0, 1) * (1 / (d.n_users + 1)**2)
         
         F = F1 + F2 + F3 + F4
         if F.length() > 0.001 :
             stable = False
             dt = F.normalize()
         
-            d += dt
+            d.p += dt
     
-    if stable == True and non_con_user > 10:
+    if stable == True and non_con_user > 10 and drone_n < 4 :
         
         stable = False
         drone_n += 1
-        d = pygame.Vector3(50 , 50 , 20)
-        drone.insert(0, d)
+        d = drone(theta)
+        drone_l.insert(0, d)
     
     #print(drone_n)
     
-    for i, d in enumerate(drone):
-        
-        try:
-           r = d.z / np.tan(np.radians(theta))
-           #print(i, ":", d.z)
-        except:
-            #print(i, ":", d.z)
-            if d.z > 300 :
-               drone.pop(0)
-               drone_n -= 1
-               non_con = non_con_user
-            #print(drone_n)
+    for i, d in enumerate(drone_l):
 
+        print(d.p.z)
         
-        pygame.draw.circle(screen, (0, 0 , 50), [d.x, d.y], r)
-        if d.z > 0 :
-           pygame.draw.circle(screen, (0, 0 ,255), [d.x, d.y], 5)
+        pygame.draw.circle(screen, (0, 0 , 50), [d.p.x, d.p.y], d.r())
+        if d.p.z > 0 :
+           pygame.draw.circle(screen, (0, 0 ,255), [d.p.x, d.p.y], 5)
         else :
-            pygame.draw.circle(screen, (255, 255, 255), [d.x, d.y], 5)
+            pygame.draw.circle(screen, (255, 255, 255), [d.p.x, d.p.y], 5)
         
     for p in pop :
         if p[1] == 0:
