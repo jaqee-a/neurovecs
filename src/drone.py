f2ee5c3aa668670f4b18dc01c10b252f3a07ae02
roumaLch <93671096+roumaLch@users.noreply.github.com>
2022-05-17 09:15:41 +0100

    Update drone.py

--- src/drone.py
+++ src/drone.py
@@ -1,6 +1,8 @@
+from turtle import distance
 import pygame
 import numpy as np
 
+dist = 70
 
 class drone :
 
@@ -9,6 +11,7 @@ class drone :
         self.p = pygame.Vector3(50, 50, 30)
         self.theta = theta
         self.n_users = 0
+        self.pt = 30
 
     def r(self) :
         
@@ -16,4 +19,32 @@ class drone :
 
     def distance(self) :
 
-        return np.sqrt(self.r()**2 + self.p.z**2)
+        return min(dist, np.sqrt(self.r()**2 + self.p.z**2))
+
+    def SNR(self, d) :
+
+        T = 2 # db
+        sig = 1 / 10**6 # Watts
+        u = 9.61
+        b = 0.16
+        nlos = 1
+        nNlos = 20
+        fc = 2.5 * 10**9 # GHz
+        c = 299792458 # m/s
+        a = 2 
+
+        r1 = np.sqrt(d**2 - self.p.z**2)
+    
+        plos2 = 1 / (1 + u * np.exp( -1 * b * (np.arctan(self.p.z / r1) - u)))
+
+        plos = 1 / (1 + u * np.exp( -1 * b * (180/np.pi * np.arctan(self.p.z / r1) - u)))
+        pNlos = 1 - plos
+
+        lOS = 20*np.log10(4*np.pi*fc*d / c) + nlos
+        nLOS = 20*np.log10(4*np.pi*fc*d / c) + nNlos
+
+        l = lOS * plos + nLOS * pNlos
+
+        pr = self.pt - l
+        
+        return pr
