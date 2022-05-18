76145da91b41fe5ebc10cd9c0bcf174893908c88
roumaLch <93671096+roumaLch@users.noreply.github.com>
2022-05-18 11:39:21 +0100

    Create readMap.py

---
+++ src/readMap.py
@@ -0,0 +1,25 @@
+import pygame
+
+def read() :
+    obs = []
+    i, j = 0, 0
+    f = open("map", 'r')
+    
+    for line in f :
+        row = []
+        for c in line :
+            if c == 'X' :
+                obs.append([pygame.Vector3(i, j, 0), [32, 32]])
+            i += 32
+        i = 0
+        j += 32
+        
+    f.close()
+    return obs
+
+def draw(screen, wall, obs):
+
+    for ob in obs :
+        screen.blit(wall, (ob[0].x,ob[0].y))
+
+    
