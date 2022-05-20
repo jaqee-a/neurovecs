e9a82ee12c5a7628844d9b97b3abc6cb0d0779f2
roumaLch <93671096+roumaLch@users.noreply.github.com>
2022-05-20 18:27:48 +0100

    Update drone.py

--- src/drone.py
+++ src/drone.py
@@ -21,31 +21,4 @@ class Drone :
 
         return min(dist, np.sqrt(self.r()**2 + self.p.z**2))
 
-    def SNR(self, d) :
-
-        T = 2 # db
-        sig = 1 / 10**6 # Watts
-        u = 9.61
-        b = 0.16
-        nlos = 1
-        nNlos = 20
-        fc = 2.5 * 10**9 # GHz
-        c = 299792458 # m/s
-        a = 2 
-
-        r1 = np.sqrt(d**2 - self.p.z**2)
     
-        plos2 = 1 / (1 + u * np.exp( -1 * b * (np.arctan(self.p.z / r1) - u)))
-
-        plos = 1 / (1 + u * np.exp( -1 * b * (180/np.pi * np.arctan(self.p.z / r1) - u)))
-        pNlos = 1 - plos
-
-        lOS = 20*np.log10(4*np.pi*fc*d / c) + nlos
-        nLOS = 20*np.log10(4*np.pi*fc*d / c) + nNlos
-
-        l = lOS * plos + nLOS * pNlos
-
-        pr = self.pt - l
-        
-        return pr
-
