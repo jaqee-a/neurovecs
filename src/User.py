1e563d656353e51429e40f5ba13beb25350e9519
roumaLch <93671096+roumaLch@users.noreply.github.com>
2022-05-21 00:17:33 +0100

    Update User.py

--- src/User.py
+++ src/User.py
@@ -1,7 +1,8 @@
 import random
 from tkinter import W
-from math import sqrt
+from math import log2, sqrt
 from turtle import isvisible
+from cv2 import log
 import pygame
 import numpy as np
 
@@ -11,9 +12,13 @@ class user :
     def __init__(self, velocity, height, width, obs) -> None:
         
         self.height, self.width = height, width
-        self.u = pygame.Vector3(random.random() * 704 * 0.5 + 256, random.random() * 320 * 0.5 + 320, 0)
+        a = random.choice([0,1])
+        if a == 0 :
+           self.u = pygame.Vector3(random.random() * 300 * 0.5 + 100, random.random() * 320 * 0.5 + 320, 0)
+        else :
+           self.u = pygame.Vector3(random.random() * 300 * 0.5 + 450, random.random() * 320 * 0.5 + 320, 0) 
         self.v = velocity
-        self.d = 0
+        
         self.theta = 0
         self.beta = 0
         self.delta_theta = 0.3
@@ -60,27 +65,25 @@ class user :
         sig = 1 / (10**6) # Watts
         u = 9.61
         b = 0.16
-        nlos = 1
-        nNlos = 20
+        nlos = 1 #dB
+        nNlos = 20 #dB
         fc = 2.5 * 10**9 # Hz
         c = 299792458 # m/s
         a = 2 
-
-        r1 = np.sqrt(d**2 - self.p.z**2)
+        
+        dist = d.p.distance_to(self.u)
+        r1 = np.sqrt(dist**2 - d.p.z**2)
     
-        plos2 = 1 / (1 + u * np.exp( -1 * b * (np.degrees(np.arctan(self.p.z / r1)) - u)))
+        plos = 1 / (1 + u * np.exp( -1 * b * (np.degrees(np.arctan(d.p.z / r1)) - u)))
 
-        plos = 1 / (1 + u * np.exp( -1 * b * (180/np.pi * np.arctan(self.p.z / r1) - u)))
         pNlos = 1 - plos
 
-        print(plos, plos2)
-
-        lOS = 20*np.log10(4*np.pi*fc*d / c) + nlos
-        nLOS = 20*np.log10(4*np.pi*fc*d / c) + nNlos
+        lOS = 20*np.log10(4*np.pi*fc*dist / c) + nlos
+        nLOS = 20*np.log10(4*np.pi*fc*dist / c) + nNlos
 
         l = lOS * plos + nLOS * pNlos
 
-        pr = self.pt - l
+        pr = d.pt - l
         
-        return pr
+        print(pr / sig)
 
