82a5736b321b0bb49fa05dfea07bab193b8f25fc
roumaLch <93671096+roumaLch@users.noreply.github.com>
2022-05-29 11:43:43 +0100

    Update drone.py

--- src/drone.py
+++ src/drone.py
@@ -1,18 +1,28 @@
 from turtle import distance
 import pygame
 import numpy as np
+import random
+
+from regex import R
 
 class Drone :
 
+    color = [(255,0,0), (0,255,0), (0,0,255), (255,255,0), (0,255,255), (255,0,255), (50,200,0), (50,0,200), (20,20,100), (0,20,100)]
+    i = 0
+
     def __init__ (self) :
 
-        self.p = pygame.Vector3(800, 60, 20)
+        self.p = pygame.Vector3(800, 60, 100)
         self.theta = 0
         self.n_users = 0
         self.pt = 5 #Watt
         self.band = 100 * 10**6 #Hz
         self.con = []
         self.theta = 42.44
+        self.capacity = 20
+        self.color = Drone.color[Drone.i]
+        Drone.i += 1
+
 
     def r(self) :
         
@@ -27,6 +37,11 @@ class Drone :
         avr = 0
         
         for p in self.con :
-            avr += p.SNR(self)
+            avr += p.SNR(self)[1]
         
         return avr / len(self.con)
+
+    def show(self, screen, font) :
+
+        f = font.render(str(int(self.p.z/6)), 1, pygame.Color("coral"))
+        screen.blit(f, (self.p.x,self.p.y))
