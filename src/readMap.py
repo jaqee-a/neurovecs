48e1e9be8a2d8bf6d53f37b1192affd00909f134
roumaLch <93671096+roumaLch@users.noreply.github.com>
2022-06-02 05:37:40 +0100

    Update readMap.py

--- src/readMap.py
+++ src/readMap.py
@@ -3,13 +3,13 @@ import pygame
 def read() :
     obs = []
     i, j = 0, 0
-    f = open("src\map2", 'r')
+    f = open("src\map", 'r')
     
     for line in f :
         for c in line :
             if c == 'X' :
-                for n in range(10) :
-                    obs.append([pygame.Vector3(i, j, n*10), [32, 32]])
+                for n in range(200) :
+                    obs.append([pygame.Vector3(i, j, n), [32, 32]])
             i += 32
         i = 0
         j += 32
@@ -20,6 +20,7 @@ def read() :
 def draw(screen, wall, obs):
 
     for ob in obs :
-        screen.blit(wall, (ob[0].x,ob[0].y))
+        if ob[0].z == 0 :
+           screen.blit(wall, (ob[0].x,ob[0].y))
 
     
