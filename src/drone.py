1ec4ce8abcec5e1a89546c2657d3a3b81698ef03
roumaLch <93671096+roumaLch@users.noreply.github.com>
2022-06-15 02:07:49 +0100

    Update neurovec3D.py

--- src/drone.py
+++ src/drone.py
@@ -10,9 +10,9 @@ class Drone :
     color = [(255,0,0), (0,255,0), (0,0,255), (255,255,0), (0,255,255), (255,0,255), (50,200,0), (50,0,200), (20,20,100), (0,20,100)]
     i = 0
     
-    batteryCapacity = 8000 #mAh
+    batteryCapacity = 22000 #mAh
     capacity = batteryCapacity
-    batteryVoltage = 3.2 #v
+    batteryVoltage = 51.8 #v
 
     def __init__ (self) :
 
