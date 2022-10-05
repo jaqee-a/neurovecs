9da2517db675954bf8bd993b682f3562740cbe82
roumaLch <93671096+roumaLch@users.noreply.github.com>
2022-10-05 10:27:08 +0100

    Update main.py

--- examples/main.py
+++ examples/main.py
@@ -1,13 +1,6 @@
 from game import Game
-from multidrone import GameMultiDrone
-from simulation.imguiapp import ImGuiApp
 
-imGuiApp : ImGuiApp()
 
-if __name__ == "__main__":
-    #GameMultiDrone()
+if __name__ == "__main__" :
     Game()
-    print("fff")
-    print(imGuiApp.selectedCamouflageMode)
-    if imGuiApp.selectedCamouflageMode == 4 :
-        print("fff")
+    
