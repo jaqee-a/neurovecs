a529ac5ebeb90a568bb860866813993f0215e962
roumaLch <93671096+roumaLch@users.noreply.github.com>
2022-10-05 10:27:15 +0100

    Update obstacle.py

--- examples/obstacle.py
+++ examples/obstacle.py
@@ -12,6 +12,8 @@ import core.time
 class obstacle:
 
     def __init__(self, i, j, h, app, mesh, type) :
+
+        self.m_app = app
         
         self.position = glm.vec3(6 + i * 12, h, 6 + j * 12)
         self.obstacle = self.generateFromMesh(mesh, self.position, app)
@@ -19,12 +21,15 @@ class obstacle:
         self.type = type
 
 
-    def generateFromMesh(self, mesh: core.components.mesh.Mesh, position, app):
+    def generateFromMesh(self, mesh: core.components.mesh.Mesh, position, app) :
         obj = app.m_ActiveScene.makeEntity()
         obj.linkComponent(mesh)
         obj.addComponent(core.components.transform.Transform, *position, *([0]*3))
 
         return obj
+
+    def destroy(self) :
+        self.m_app.m_ActiveScene.m_Registry.removeEntity(self.obstacle)
     
 
 
